5cc30b91598fb01cac362c6cf5a97d1b6f44a772
YoshiS214 <60101499+yoshis214@users.noreply.github.com>
2020-09-21 15:36:59 +0100

    Add widgets in activity_main.xml Declare widgets in MainActivity.kt

--- app/src/main/java/com/example/datasamplingapp/MainActivity.kt
+++ app/src/main/java/com/example/datasamplingapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.datasamplingapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textNote = findViewById<EditText>(R.id.text_note) as EditText
+        val startButton = findViewById<Button>(R.id.start_record) as Button
+        val stopButton = findViewById<Button>(R.id.stop_record) as Button
+
+        startButton.setOnClickListener { v ->
+            recordMotion()
+        }
+    }
+
+    fun recordMotion(){
+
     }
 }
