05831d56894f20d678c572afed4e8974eb27cd34
YoshiS214 <60101499+YoshiS214@users.noreply.github.com>
2020-09-24 21:32:13 +0100

    Add createCSV Change writeCSV Now it can write a csv file

--- app/src/main/java/com/example/datasamplingapp/MainActivity.kt
+++ app/src/main/java/com/example/datasamplingapp/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.datasamplingapp
 
 import android.content.Context
+import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import kotlin.properties.Delegates
 import java.util.Calendar
 import java.io.File
+import java.io.FileOutputStream
+import java.net.URI
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
@@ -20,6 +25,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private var linearAccelerations : ArrayList<FloatArray>? = null
     private var orientations : ArrayList<FloatArray>? = null
     private var time: Calendar by Delegates.notNull<Calendar>()
+    private var uri : Uri by Delegates.notNull<Uri>()
 
 
 
@@ -34,18 +40,32 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         val startButton = findViewById<Button>(R.id.start_record) as Button
         val stopButton = findViewById<Button>(R.id.stop_record) as Button
 
+        textNote.setText(textNote.text.toString())
+
+        disableSensor()
+        createCSV()
+
 
         startButton.setOnClickListener { v ->
+
+            initialise()
             enableSensor()
         }
 
         stopButton.setOnClickListener {v ->
             disableSensor()
+
             writeCSV(textNote.text.toString(), null)
             writeCSV("Acceleration", accelerations)
             writeCSV("Linear Acceleration", linearAccelerations)
             writeCSV("Orientation", orientations)
         }
+    }
+
+    private fun initialise(){
+        accelerations?.clear()
+        linearAccelerations?.clear()
+        orientations?.clear()
     }
 
     private fun enableSensor(){
@@ -57,6 +77,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private fun disableSensor(){
         sensorManager.unregisterListener(this)
+
     }
 
     override fun onResume() {
@@ -88,6 +109,23 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     }
 
+    private fun createCSV(){
+        val intent: Intent = Intent(Intent.ACTION_CREATE_DOCUMENT)
+        intent.type = "text/csv"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.putExtra("data.csv", "")
+        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, URI.create("/storage/emulated/0/Download"))
+        startActivityForResult(intent, 32)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, resultData)
+        resultData?.data?.also{uriOfData: Uri ->
+            uri = uriOfData
+        }
+    }
+
     private fun writeCSV(sensor: String, data: ArrayList<FloatArray>?){
         var txt = "Time,X-value,Y-value,Z-value"
         if (data != null) {
@@ -96,7 +134,13 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             }
         }
         txt = sensor + "\n" + txt
-        File(applicationContext.filesDir, "SensorData.csv").appendText(txt)
+
+        contentResolver.openFileDescriptor(uri, "w")?.use{
+            FileOutputStream(it.fileDescriptor).use{
+                it.write(txt.toByteArray())
+            }
+        }
+
     }
 
 }
